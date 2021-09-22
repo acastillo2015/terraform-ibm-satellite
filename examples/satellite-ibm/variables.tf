d62e2d6f406d81a6785b63e226cb08ea0f204509
acastillo2015 <acastillo@cl.ibm.com>
2021-09-22 09:36:26 -0300

    Update variables.tf

--- examples/satellite-ibm/variables.tf
+++ examples/satellite-ibm/variables.tf
@@ -45,7 +45,7 @@ variable "location_bucket" {
 variable "is_location_exist" {
   description = "Determines if the location has to be created or not"
   type        = bool
-  default     = yes
+  default     = true
 }
 
 variable "host_labels" {
